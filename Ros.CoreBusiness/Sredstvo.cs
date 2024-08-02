aecd8fa537632cd21c327f13f64b8c6b92492495
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-08-02 12:54:53 +0200

    Model Sredstvo

---
+++ Ros.CoreBusiness/Sredstvo.cs
@@ -0,0 +1,21 @@
+﻿namespace Ros.CoreBusiness;
+
+public class Sredstvo
+{
+	public int Id { get; set; }
+	public string? SerijskiBroj { get; set; }
+	public string? Naziv { get; set; }
+	public string? NadredjenoSredstvo { get; set; }
+	public string? SAPInventarniBroj { get; set; }
+
+	// TODO
+	// public string? Kategorija { get; set; }
+	// public int Zaduzilac { get; set; }
+	// public string? Ime { get; set; }
+	// public string? Prezime { get; set; }
+	// public string? Username { get; set; }
+	// public DateOnly DatumZaduzenja { get; set; }
+	// public string? OpisLokacije { get; set; }
+	// public string? NajnizaOJ { get; set; }
+	// public string? NazivRM { get; set; }
+}
