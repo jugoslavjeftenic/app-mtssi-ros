e0a1b9beb390865e3206d99e6a3ea19a70a44e3b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-08-04 13:30:21 +0200

    Base ViewModel class

---
+++ Ros.Maui/ViewModel/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Ros.Maui.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
+{
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(IsNotBusy))]
+	private bool _isBusy;
+
+	[ObservableProperty]
+	private string _title = string.Empty;
+
+	public bool IsNotBusy => !IsBusy;
+}
