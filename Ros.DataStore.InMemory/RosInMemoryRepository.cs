57e4fab883058dcc3b9549cf4784b7b4aa4805bf
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-08-04 13:29:44 +0200

    In memomry repository

---
+++ Ros.DataStore.InMemory/RosInMemoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ros.CoreBusiness;
+
+namespace Ros.DataStore.InMemory;
+
+public class RosInMemoryRepository
+{
+	List<Sredstvo> _sredstva = [];
+
+	public async Task<List<Sredstvo>> GetSredstva()
+	{
+		return _sredstva;
+	}
+}
