1f935ad58aea57a4c034a72127814ec7c518f857
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-08-04 13:51:59 +0200

    Sredstvo refactored to Asset

---
+++ Ros.CoreBusiness/Asset.cs
@@ -0,0 +1,21 @@
+﻿namespace Ros.CoreBusiness;
+
+public class Asset
+{
+	public int Id { get; set; }
+	public string? SerialNumber { get; set; }
+	public string? Name { get; set; }
+	public string? ParentAsset { get; set; }
+	public string? SAPInventoryNumber { get; set; }
+
+	// TODO
+	// public string? Category { get; set; }
+	// public int Assignee { get; set; }
+	// public string? FirstName { get; set; }
+	// public string? LastName { get; set; }
+	// public string? Username { get; set; }
+	// public DateOnly AssignmentDate { get; set; }
+	// public string? LocationDescription { get; set; }
+	// public string? LowestOrganizationalUnit { get; set; }
+	// public string? ResourceName { get; set; }
+}
