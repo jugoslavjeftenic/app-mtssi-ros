cfde81f8aa0e86eb3bebb2abd0818673f55145da
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-08-05 10:00:20 +0200

    AsswtsViewModel binded to MainPage

--- Ros.Maui/View/MainPage.xaml.cs
+++ Ros.Maui/View/MainPage.xaml.cs
@@ -1,9 +1,12 @@
+using Ros.Maui.ViewModel;
+
 namespace Ros.Maui.View;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+	public MainPage(AssetsViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
 	}
 }
