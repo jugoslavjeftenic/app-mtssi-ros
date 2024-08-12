509ed35d99e7802cc2edcb274c6a26451959bce2
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-08-12 22:35:42 +0200

    New Asset fields

--- Ros.Maui/Models/Asset.cs
+++ Ros.Maui/Models/Asset.cs
@@ -3,19 +3,17 @@
 public class Asset
 {
 	public int Id { get; set; }
-	public string? SerialNumber { get; set; }
-	public string? Name { get; set; }
 	public string? ParentAsset { get; set; }
+	public string? Name { get; set; }
+	public string? Category { get; set; }
 	public string? SAPInventoryNumber { get; set; }
-
-	// TODO
-	// public string? Category { get; set; }
-	// public int Assignee { get; set; }
-	// public string? FirstName { get; set; }
-	// public string? LastName { get; set; }
-	// public string? Username { get; set; }
-	// public DateOnly AssignmentDate { get; set; }
-	// public string? LocationDescription { get; set; }
-	// public string? LowestOrganizationalUnit { get; set; }
-	// public string? ResourceName { get; set; }
+	public int Assignee { get; set; }
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	public string? Username { get; set; }
+	public DateOnly AssetAssignmentDate { get; set; }
+	public string? SerialNumber { get; set; }
+	public string? LocationDescription { get; set; }
+	public string? LowestOrganizationalUnit { get; set; }
+	public string? JobTitle { get; set; }
 }
